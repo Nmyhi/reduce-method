0769ae7efb53a36b2af6eea8b2b0a48674640f04
Mihaly Lovrencsics <lomysa@gmail.com>
2023-08-19 10:13:43 +0000

    initial commit, practice reduce method

--- reduce.js
+++ reduce.js
@@ -6,7 +6,10 @@
 
 
 // Summing an array of numbers:
+let nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
 
+console.log(sum);
 
 const teamMembers = [
   {
@@ -28,10 +31,29 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Pityu',
+    profession: 'Takony',
+    yrsExperience: 30
   }
 ];
 
 // Totaling a specific object property
 
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if (!acc[key]) {
+      acc[key] = curr.yrsExperience;
+    } else {
+      acc[key] += curr.yrsExperience;
+    }
+    return acc;
+}, {});
+
+console.log(experienceByProfession);
